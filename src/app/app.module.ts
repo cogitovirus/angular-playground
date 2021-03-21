903d6e8dac77e53b309386d8427bd05fff55ab62
wzolni <w.zolnierowicz@gmail.com>
2021-03-21 18:34:08 +0100

    CGV-3 book-section continues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,11 +36,9 @@ import { BookReviewComponent } from './components/review-section/book-review/boo
     AppRoutingModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatCardModule
-  ],
-  exports: [
-    MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
